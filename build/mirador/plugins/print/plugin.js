7aee50eba04abe35acd579bfa084083a0d704978
Erica Hayes <eyhayes@ncsu.edu>
2019-03-05 13:08:07 -0500

    Updated mirador folder

--- build/mirador/plugins/print/plugin.js
+++ build/mirador/plugins/print/plugin.js
@@ -1,32 +1,38 @@
-/**
- * plugin.js
- *
- * Copyright, Moxiecode Systems AB
- * Released under LGPL License.
- *
- * License: http://www.tinymce.com/license
- * Contributing: http://www.tinymce.com/contributing
- */
+(function () {
+var print = (function () {
+  'use strict';
 
-/*global tinymce:true */
+  var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-tinymce.PluginManager.add('print', function(editor) {
-	editor.addCommand('mcePrint', function() {
-		editor.getWin().print();
-	});
+  var register = function (editor) {
+    editor.addCommand('mcePrint', function () {
+      editor.getWin().print();
+    });
+  };
+  var $_6xny5djkjm0ofzuk = { register: register };
 
-	editor.addButton('print', {
-		title: 'Print',
-		cmd: 'mcePrint'
-	});
+  var register$1 = function (editor) {
+    editor.addButton('print', {
+      title: 'Print',
+      cmd: 'mcePrint'
+    });
+    editor.addMenuItem('print', {
+      text: 'Print',
+      cmd: 'mcePrint',
+      icon: 'print'
+    });
+  };
+  var $_9tduozjljm0ofzul = { register: register$1 };
 
-	editor.addShortcut('Ctrl+P', '', 'mcePrint');
+  global.add('print', function (editor) {
+    $_6xny5djkjm0ofzuk.register(editor);
+    $_9tduozjljm0ofzul.register(editor);
+    editor.addShortcut('Meta+P', '', 'mcePrint');
+  });
+  function Plugin () {
+  }
 
-	editor.addMenuItem('print', {
-		text: 'Print',
-		cmd: 'mcePrint',
-		icon: 'print',
-		shortcut: 'Ctrl+P',
-		context: 'file'
-	});
-});
+  return Plugin;
+
+}());
+})();
